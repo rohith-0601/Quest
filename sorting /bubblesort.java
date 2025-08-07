9c2f36c4a34c22dc9d024a45e802cae81279b7f2
rohith-0601 <rohithperugu3@gmail.com>
2025-08-07 10:42:48 +0530

    react and anguilar diff

---
+++ sorting /bubblesort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class bubblesort {
+    public static void Bubblesort(int[] arr){
+        int n = arr.length;
+
+        for(int i=0;i<n-1;i++){
+            for(int j = 0;j<n-i-1;j++){
+                if(arr[j+1] < arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,5,6,3,4,2};
+        Bubblesort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
---
+++ sorting /insertionsort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class insertionsort {
+    public static void Insertionsort(int[] arr){
+        int n = arr.length;
+
+
+        for(int i = 1;i<n;i++){
+            int key = arr[i];
+            int j = i-1;
+
+            while(j >=0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+    public static void main(String[] args){
+        int[] arr = {1,5,6,3,4,2};
+        Insertionsort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
